048e11705941d6286994bde0f815a64e0afea8ef
larilampen <lari.lampen@gmail.com>
2014-04-01 23:25:09 +0200

    Replaced old NSContext class with new, complete implementation (copied from OAI Harvest Manager).

--- src/main/java/nl/mpi/mdmapper/NSContext.java
+++ src/main/java/nl/mpi/mdmapper/NSContext.java
@@ -22,6 +22,7 @@ import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
@@ -29,28 +30,48 @@ import javax.xml.namespace.NamespaceContext;
 import javax.xml.XMLConstants;
 
 /**
- * A simple implementation of an XML namespace context.
+ * An implementation of an XML namespace context.
  *
  * @author Lari Lampen (MPI-PL)
  */
-
 public class NSContext implements NamespaceContext {
     private static final Logger logger = Logger.getLogger(NSContext.class);
 
-    // Two maps are used to make it simple to map from prefixes to
-    // URIs and vice versa.
+    /**
+     * URIs indexed by prefix.
+     */
     private Map<String, String> pref2ns;
-    private Map<String, String> ns2pref;
 
+    /**
+     * Prefixes indexed by URI. (Note that multiple prefixes can be bound to
+     * the same URI, but a prefix is only bound to a single URI at a time.
+     */
+    private Map<String, List<String>> ns2pref;
+
+    /**
+     * Create a new namespace context (with no bindings).
+     */
     public NSContext() {
-	pref2ns = new HashMap<String, String>();
-	ns2pref = new HashMap<String, String>();
+	pref2ns = new HashMap<>();
+	ns2pref = new HashMap<>();
     }
 
+    /**
+     * Add a new namespace binding.
+     *
+     * @param prefix namespace prefix
+     * @param ns namespace address
+     */
     public void add(String prefix, String ns) {
-	logger.debug("add "+prefix+", "+ns);
-	ns2pref.put(ns, prefix);
 	pref2ns.put(prefix, ns);
+	List<String> prefixes;
+	if (ns2pref.containsKey(ns)) {
+	    prefixes = ns2pref.get(ns);
+	} else {
+	    prefixes = new ArrayList<>();
+	    ns2pref.put(ns, prefixes);
+	}
+	prefixes.add(prefix);
     }
 
     /**
@@ -59,8 +80,6 @@ public class NSContext implements NamespaceContext {
      */
     @Override
     public String getNamespaceURI(String prefix) {
-	//	logger.debug("received request for prefix: " + prefix);
-
 	if (prefix == null)
 	    throw new IllegalArgumentException("Illegal check of null namespace prefix");
 
@@ -78,16 +97,16 @@ public class NSContext implements NamespaceContext {
     /**
      * Look up prefix based on namespace URI. Some of the return
      * values are fixed by the XML standard.
+     *
+     * @param uri namespace URI
      */
     @Override
     public String getPrefix(String uri) {
-	//	logger.debug("received request for uri: " + uri);
-
 	if (uri == null)
 	    throw new IllegalArgumentException("Illegal check of null namespace URI");
 
-	if (pref2ns.containsKey(uri))
-	    return pref2ns.get(uri);
+	if (ns2pref.containsKey(uri))
+	    return ns2pref.get(uri).get(0);
 
 	if (uri.equals(XMLConstants.XML_NS_URI))
 	    return XMLConstants.XML_NS_PREFIX;
@@ -98,24 +117,28 @@ public class NSContext implements NamespaceContext {
     }
 
     /**
-     * NOTE. Many prefixes can be bound to the same URI, e.g.
-     *
-     * <pre>
-     * <parent xmlns:prefix1="http://Namespace-name-URI">
-     *   <child xmlns:prefix2="http://Namespace-name-URI">
-     *     ...
-     *   </child>
-     * </parent>
-     * </pre>
-     *
-     * However, for simplicity this class does not implement multiple
-     * prefixes. Hence this method always returns an iterator into a
-     * list of one.
+     * Get list of prefixes bound to a namespace URI. Some of the return values
+     * are fixed by the XML standard.
      */
     @Override
     public Iterator getPrefixes(String uri) {
-	List<String> dummy = new ArrayList<>(1);
-	dummy.add(getPrefix(uri));
-	return dummy.iterator();
+	if (uri == null)
+	    throw new IllegalArgumentException("Illegal check of null namespace URI");
+
+	if (ns2pref.containsKey(uri))
+	    return ns2pref.get(uri).iterator();
+
+	if (uri.equals(XMLConstants.XML_NS_URI)) {
+	    List<String> dummy = new ArrayList<>(1);
+	    dummy.add(XMLConstants.XML_NS_PREFIX);
+	    return dummy.iterator();
+	}
+	if (uri.equals(XMLConstants.XMLNS_ATTRIBUTE_NS_URI)) {
+	    List<String> dummy = new ArrayList<>(1);
+	    dummy.add(XMLConstants.XMLNS_ATTRIBUTE);
+	    return dummy.iterator();
+	}
+
+	return Collections.emptyIterator();
     }
 }
